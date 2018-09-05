27dc43f6820727c68ae0f342ec257f7fa02a7141
Tom Hedges <tom@tomhedges.com>
2018-09-05 01:00:15 +0100

    Refactored basic artefact search by ID and display classes, and added .gitignore

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const SEARCH_INPUTBOX = "search_inputbox"
 const SEARCH_SOURCE_SELECT = "search_source_select"
+const SEARCH_INPUTBOX = "search_inputbox"
+const SEARCH_BUTTON = "search_button"
 const SOURCES = {
   V_AND_A: "v_and_a",
   BRITMUS: "british_museum",
   OTHER: "other"
 }
-//const SOURCE_RESPONSE = {
-//  SUCCESS: true,
-//  FAILURE: false,
-//}
 const dataRequestStatus = {
   NONE_MADE: "none",
   LOADING: "loading",
@@ -24,50 +21,48 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      requestExternalData: false,
-      isLoaded: false,
-      errorMessage: null,
-      //dataLoaded: false,
-      primaryImageURL: null,
+      selectedSource: SOURCES.V_AND_A,
+      lastSearchSource : null,
+      dataRequestStatus: dataRequestStatus.NONE_MADE,
+      objectData: [],
       objectID: null,
-      source: SOURCES.V_AND_A,
-      object_data: [],
-      dataRequestStatus: dataRequestStatus.NONE_MADE
+      errorMessage: null,
     }
 
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
-    this.reset = this.reset.bind(this);
   }
 
-  handleClick() {
-    //alert(this.state.objectID);
-    if (this.state.objectID) {
-      this.setState({
-        requestExternalData: true,
-        dataRequestStatus: dataRequestStatus.LOADING,
-        errorMessage: null,
-      });
+  handleClick(event) {
+    switch (event.target.id) {
+      case SEARCH_BUTTON:
+        if (this.state.objectID) {
+          this.setState({
+            dataRequestStatus: dataRequestStatus.LOADING,
+            errorMessage: null,
+            lastSearchSource: this.state.selectedSource,
+          });
+          this.buildUrl();
+        } //else there is no search term, so no action. oin future, have button disabled until there is a term.
+      break;
 
-      //// TESTING!
-      this.buildUrl();
-      ////
-
-    } //else there is no search term, so no action. oin future, have button disabled until there is a term.
+      default:
+      break;
+    }
   }
 
   handleChange(event) {
     switch (event.target.id) {
       case SEARCH_INPUTBOX:
-      this.setState({
-        objectID: event.target.value
-      })
+        this.setState({
+          objectID: event.target.value
+        })
       break;
 
       case SEARCH_SOURCE_SELECT:
-      this.setState({
-        source: event.target.value
-      });
+        this.setState({
+          selectedSource: event.target.value
+        });
       break;
 
       default:
@@ -78,7 +73,7 @@ class App extends React.Component {
   buildUrl() {
     let finalURL = null;
 
-    switch (this.state.source) {
+    switch (this.state.selectedSource) {
       case SOURCES.V_AND_A:
       finalURL = "https://www.vam.ac.uk/api/json/museumobject/" + this.state.objectID
       break;
@@ -104,68 +99,20 @@ class App extends React.Component {
       .then(
         (result) => {
           this.setState({
-            isLoaded: true,
             dataRequestStatus: dataRequestStatus.SUCCESS,
-            object_data: result,
+            objectData: result,
           });
-          this.updateFields();
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
           this.setState({
-            isLoaded: true,
-            //error: error,
             dataRequestStatus: dataRequestStatus.FAILURE,
             errorMessage: error.message,
           });
-          this.updateFields();
         }
       )
-  }
-
-  updateFields() {
-    if (this.state.dataRequestStatus === dataRequestStatus.SUCCESS) {
-      switch (this.state.source) {
-        case SOURCES.V_AND_A:   
-          let imageURL = null;
-          if (this.state.object_data[0].fields.primary_image_id) {
-            imageURL = 'http://media.vam.ac.uk/media/thira/collection_images/' + this.state.object_data[0].fields.primary_image_id.substring(0, 6) + '/' + this.state.object_data[0].fields.primary_image_id + '.jpg'
-          };
-          this.setState({
-            primaryImageURL: imageURL,
-            requestExternalData: false,
-          });
-        break;
-
-        case SOURCES.BRITMUS:
-        break;
-
-        case SOURCES.OTHER:
-        break;
-
-        default:
-        break;
-      }
-    } else {
-      this.setState({
-        primaryImageURL: null,
-        requestExternalData: false,
-        object_data: [],
-      });
-    }
-  }
-
-  //reset(url) {
-  reset() {
-    this.setState({
-      //dataLoaded: true,
-      requestExternalData: false,
-      primaryImageURL: null,
-      isLoaded: false,
-      errorMessage: null,
-    });
   }
 
   render() {
@@ -177,42 +124,63 @@ class App extends React.Component {
       </div>
       <div className = "controls">
         <p className = "App-intro" >
-          Search for an object by ID! eg. O61949
+          Search for an object by ID! eg. O61949 or O234
         </p>
-        <select id={SEARCH_SOURCE_SELECT} value={this.state.source} onChange={this.handleChange}>
+        <select id={SEARCH_SOURCE_SELECT} value={this.state.selectedSource} onChange={this.handleChange}>
           <option value={SOURCES.V_AND_A}>V&A Museum</option>
           <option value={SOURCES.BRITMUS}>British Museum</option>
           <option value={SOURCES.OTHER}>Other</option>
         </select>
-        <input type="text" id={SEARCH_INPUTBOX} value={this.state.value} onChange={this.handleChange} />
-        <button className="button" id="search" onClick={this.handleClick}>Load Artefact</button>
+        <input type="text" id={SEARCH_INPUTBOX} onChange={this.handleChange} />
+        <button className="button" id={SEARCH_BUTTON} onClick={this.handleClick}>Load Artefact</button>
       </div>
       <div className="external-data">
-        <PrimaryImageV3 dataRequestStatus={this.state.dataRequestStatus} primaryImageURL={this.state.primaryImageURL} errorMessage={this.state.errorMessage} isLoaded={this.state.isLoaded} requestExternalData={this.state.requestExternalData}/>
+        <ArtefactDetails dataRequestStatus={this.state.dataRequestStatus} source={this.state.lastSearchSource} objectData={this.state.objectData} errorMessage={this.state.errorMessage}/>
       </div>
     </div>);
     }
   }
 
-  // TAKE 2:
-  //<PrimaryImage objectID={this.state.objectID} requestExternalData={this.state.requestExternalData} reset={this.reset}/>
-        
-  // TAKE 1:
-  //<ExternalData requestExternalData={this.state.requestExternalData} objectID={this.state.objectID} reset={this.reset} />
-  //<img src={this.state.primaryImageURL} alt='test' />
-
-
-
   class ArtefactDetails extends React.Component {
-    render () {
-      return null;
+    getFields() {
+      let fields = {
+        objectName: null,
+        objectSlug: 'Slug still to be set up...',
+        objectPrimaryImageURL: null,
+        objectText: 'Text still to be set up...',
+      }
+
+      if (this.props.dataRequestStatus === dataRequestStatus.SUCCESS) {
+        switch (this.props.source) {
+          case SOURCES.V_AND_A:
+            fields.objectName = (!this.props.objectData[0].fields.title) ? "Unknown" : this.props.objectData[0].fields.title;
+            if (this.props.objectData[0].fields.primary_image_id) {
+              fields.objectPrimaryImageURL = 'http://media.vam.ac.uk/media/thira/collection_images/' + this.props.objectData[0].fields.primary_image_id.substring(0, 6) + '/' + this.props.objectData[0].fields.primary_image_id + '.jpg'
+            };
+          break;
+
+          case SOURCES.BRITMUS:
+          break;
+
+          case SOURCES.OTHER:
+          break;
+
+          default:
+          break;
+        }
+      }
+
+      return fields;
     }
-  }
 
-
-  class PrimaryImageV3 extends React.Component {
     render () {
-      let return_value = '';
+      let fields = this.getFields();
+      let objectName = fields.objectName;
+      let objectSlug = fields.objectSlug;
+      let objectPrimaryImageURL = fields.objectPrimaryImageURL;
+      let objectText = fields.objectText;
+
+      let return_value = null;
 
       switch (this.props.dataRequestStatus) {
         case dataRequestStatus.NONE_MADE:   
@@ -223,12 +191,17 @@ class App extends React.Component {
           return_value = <p>Loading...</p>;
         break;
 
-        case dataRequestStatus.SUCCESS:   
-          return_value = <img src={this.props.primaryImageURL} alt='Primary visual representation of artefact' />;
-        break;
-
         case dataRequestStatus.FAILURE:
           return_value = <p>Error: {this.props.errorMessage}</p>
+        break;
+
+        case dataRequestStatus.SUCCESS:
+          return_value = (<div>
+              <ArtefactName objectName={objectName} />
+              <ArtefactSlug objectSlug={objectSlug}/>
+              <ArtefactPrimaryImage objectPrimaryImageURL={objectPrimaryImageURL}/>
+              <ArtefactText objectText={objectText}/>
+          </div>);
         break;
 
         default:
@@ -237,205 +210,41 @@ class App extends React.Component {
       }
 
       return return_value;
+    }
+  }
 
-      /*
-      if (this.props.errorMessage) {
-        return <p>Error: {this.props.errorMessage}</p>
+  class ArtefactName extends React.Component {
+    render () {
+      return (
+        <h2>Name: {this.props.objectName}</h2>
+      );
+    }
+  }
+
+  class ArtefactSlug extends React.Component {
+    render () {
+      return (
+        <p>ArtefactSlug: {this.props.objectSlug}</p>
+      );
+    }
+  }
+
+  class ArtefactPrimaryImage extends React.Component {
+    render () {
+      if (this.props.objectPrimaryImageURL) {
+        return <img src={this.props.objectPrimaryImageURL} alt='Primary visual representation of artefact' />;
       } else {
-        if (this.props.primaryImageURL) {
-          return <img src={this.props.primaryImageURL} alt='Primary visual representation of artefact' />;
-        } else {
-          if (this.props.requestExternalData) {
-            return <p>Loading...</p>
-          } else {
-            if (this.props.isLoaded) {
-              return <p>Sorry - No image available</p>
-            } else {
-              //return <p>SHOULD NEVER APPEAR???...</p>
-              return null;
-            }
-          }
-        }
+        return <p>No image available</p>;
       }
-      */
-
     }
   }
 
-
-
-
-class PrimaryImage extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      //dataLoaded: false,
-      //requestExternalData: false,
-      primaryImageURL: null,
-      foundImage: null,
-    };
-    
-    this.resetPrimaryImageLoad = this.resetPrimaryImageLoad.bind(this);
-  }
-  
-  resetPrimaryImageLoad(url) {
-    this.setState({
-      //dataLoaded: true,
-      //requestExternalData: false,
-      primaryImageURL: url,
-    });
-    //this.props.reset(url);
-    this.props.reset();
-  }
-
-  render () {
-    if (this.props.requestExternalData) {
-    //if (this.props.requestExternalData || this.props.dataLoaded) {
-    //if (this.state.primaryImageURL==null && this.props.requestExternalData) {
+  class ArtefactText extends React.Component {
+    render () {
       return (
-        <GetExternalDataNEW objectID={this.props.objectID} resetPrimaryImageLoad={this.resetPrimaryImageLoad}/>
-      );
-    } else if (this.state.primaryImageURL != null) {
-      return (
-        <img src={this.state.primaryImageURL} alt='THE NEW WAY' />
-      );
-    } else {
-      return <p>NOTHING TO SEE</p>
-      //return null;
-    }
-  }
-}
-
-class GetExternalDataNEW extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      imageURL: '',
-    };
-    //alert('gedN cons');
-  }
-
-  componentDidMount() {
-    let imageURL = '';
-    fetch("https://www.vam.ac.uk/api/json/museumobject/" + this.props.objectID)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          imageURL = 'http://media.vam.ac.uk/media/thira/collection_images/' +
-            result[0].fields.primary_image_id.substring(0, 6) + '/' + result[0].fields.primary_image_id +
-            '.jpg';
-          //alert(imageURL);
-          //alert(this.props.resetPrimaryImageLoad);
-          this.setState({
-            imageURL: imageURL,
-            isLoaded: true,
-            //items: result.items
-          });
-          this.props.resetPrimaryImageLoad(imageURL);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-          this.props.resetPrimaryImageLoad(null);
-        }
-      )
-  }
-
-  render() {
-    //const {error, isLoaded, imageURL} = this.state;
-    const {error, isLoaded} = this.state;
-    if (error) {
-      return <div>Error:{error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      //return (
-      //  <img src={imageURL} alt='Primary visual representation of artefact' />
-      //);
-      return <p>this should never be seen</p>;
-    }
-  }
-}
-
-
-
-
-
-
-function ExternalData(props) {
-  if (props.requestExternalData || props.dataLoaded) {
-    return (
-      <div>
-      <GetExternalData objectID={props.objectID} reset={props.reset}/>
-      </div>
-    );
-  } else {
-    return (
-      '' //<p>nothing to see</p>
-    );
-  }
-}
-
-class GetExternalData extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      imageURL: 'x',
-      //items: []
-    };
-  }
-
-  componentDidMount() {
-    let imageURL = '';
-    fetch("https://www.vam.ac.uk/api/json/museumobject/" + this.props.objectID)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          imageURL = 'http://media.vam.ac.uk/media/thira/collection_images/' +
-            result[0].fields.primary_image_id.substring(0, 6) + '/' + result[0].fields.primary_image_id +
-            '.jpg';
-          //alert(imageURL);
-          this.setState({
-            imageURL: imageURL,
-            isLoaded: true,
-            //items: result.items
-          });
-          this.props.reset(imageURL);
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-          this.props.reset(imageURL);
-        }
-      )
-  }
-
-  render() {
-    const {error, isLoaded, imageURL} = this.state;
-    if (error) {
-      return <div>Error:{error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <img src={imageURL} alt='Primary visual representation of artefact' />
+        <p>ArtefactText: {this.props.objectText}</p>
       );
     }
   }
-}
 
 export default App
